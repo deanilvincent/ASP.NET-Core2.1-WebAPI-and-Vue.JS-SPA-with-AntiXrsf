6dd99ea36077530188cb75ad802a8afde95f7295
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-08-17 18:16:25 +0800

    initial commit

---
+++ MyWebApp.API/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ASP.NET_Core2._1_WebAPI_and_Vue.JS_SPA_with_AntiXrsf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
